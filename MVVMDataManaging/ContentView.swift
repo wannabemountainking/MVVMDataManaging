91b6e9105aba4f1fe42a61c179ee1d2dba36a12c
wannabemountainking <kdy0096@gmail.com>
2025-11-30 18:03:09 +0900

    Initial Commit

---
+++ MVVMDataManaging/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MVVMDataManaging
+//
+//  Created by YoonieMac on 11/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MVVMDataManaging/MVVMDataManagingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMDataManagingApp.swift
+//  MVVMDataManaging
+//
+//  Created by YoonieMac on 11/30/25.
+//
+
+import SwiftUI
+
+@main
+struct MVVMDataManagingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
